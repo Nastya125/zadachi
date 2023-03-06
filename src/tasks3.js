58111b6dddd2576ec726bf5c37c534606ce1b113
Nastya125 <nastyatrue@mail.ru>
2023-03-06 20:47:16 +0300

    task7

--- src/tasks3.js
+++ src/tasks3.js
@@ -111,3 +111,31 @@ function pal(str) {
  console.log(pal('Колобок')) // false
 
  // task 7
+
+ function getMaxEvenElement(arr) {
+    const newArr = [];
+    for(let i=0; i< arr.length; i++){
+        if(i % 2 === 0){
+            newArr.push(arr[i]);
+        }
+    }
+    return Math.max.apply(null, newArr);
+}
+
+console.log(getMaxEvenElement([5, -7, -1, 12, 3, 0])) // 5
+console.log(getMaxEvenElement([4, -12, 29, 6, 31, 92, -50])) // 31
+
+
+ // task 8
+
+function encode(str) {
+
+}
+
+function decode(str) {
+
+}
+
+
+console.log(encode('hello')) // h2ll4
+console.log(decode('h2ll4')) // hello
