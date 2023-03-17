bf6be2d90416c65d99af5d8902999647e75d5b15
Nastya125 <nastyatrue@mail.ru>
2023-03-17 06:51:06 +0300

    task4

---
+++ src/task4.js
@@ -0,0 +1,31 @@
+function CreateButton(textContent, color, callBack) {
+
+    this.layout ()
+    
+    this.setTextContent ()
+    
+    this.setCallBack ()
+    
+    }
+
+    class CreateInput {
+
+        constructor(placeholder, value, callBack) {
+        
+        this.placeholder = placeholder;
+        
+        this.value = value;
+        
+        this.callBack = callBack;
+        
+        }
+        
+        layout();
+        
+        setPlaceholder(text);
+        
+        setCallBack(callBack);
+        
+        }
+
+    const input = new CreateInput()
